61596c8ee46e74211982c8c1043e656957eff6d9
cstavan97 <solutionassociates100@gmail.com>
2025-07-30 20:47:36 +0530

    Update create-expenses-table.js

--- create-expenses-table.js
+++ create-expenses-table.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./db/erp.db');
+const db = new sqlite3.Database('erp.db');
 
 // Create expenses table
 db.serialize(() => {
