7b4b9671395b24e86f24157b735990b896648042
cstavan97 <solutionassociates100@gmail.com>
2025-07-30 21:37:31 +0530

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,10 +1,7 @@
+// preload.js
+
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  send: (channel, data) => {
-    ipcRenderer.send(channel, data);
-  },
-  receive: (channel, func) => {
-    ipcRenderer.on(channel, (event, ...args) => func(...args));
-  }
+  loginUser: (username, password) => ipcRenderer.invoke('login-user', username, password)
 });
